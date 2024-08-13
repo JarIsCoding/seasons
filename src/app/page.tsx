6c0a3d72fede83cb9c7bc14d0301ff2dfb893bc4
JarIsCoding <derpyderp1098@gmail.com>
2024-08-13 16:27:57 -0700

    fullscreen when clicked with a hidden x only visible when panel is clicked

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,102 @@
 'use client'
 
+import { useEffect, useState } from "react";
+
 export default function Home() {
+
+  const [currentClick, setCurrentClick] = useState<string>();
+
+  useEffect(() => {
+    console.log(currentClick)
+  }, [currentClick])
+
   return (
-    <div className="text-center py">
-      <div className="w-1/2 h-1/2 absolute left-0 top-0 bg-red-200 hoverBig origin-top-left hover:z-20 z-10 rounded-br-[100px]">
-        w
+    <div className={``}>
+      <div
+        className={` 
+        absolute left-0 top-0 
+        bg-red-200 
+        origin-top-left  
+        ${currentClick === 'TL' ? 'w-full h-full z-30' : 'w-1/2 h-1/2 hover:z-20 z-10 hoverBig rounded-br-[100px]'}
+      `}
+        onClick={() => setCurrentClick('TL')}
+      >
+        <div className={`${currentClick ? 'block' : 'hidden'}`} onClick={(e) => {
+          e.stopPropagation();
+          setCurrentClick('')
+        }
+        }>
+          x
+        </div>
+        <div className="text-center">
+          w
+        </div>
       </div>
-      <div className="w-1/2 h-1/2 absolute right-0 top-0 bg-blue-200 hoverBig origin-top-right hover:z-20 z-10 rounded-bl-[100px]">
-        w
+
+      <div
+        className={`
+        absolute right-0 top-0 
+        bg-blue-200 
+        origin-top-right 
+        ${currentClick === 'TR' ? 'w-full h-full z-30' : 'w-1/2 h-1/2 hover:z-20 z-10 hoverBig rounded-bl-[100px]'}
+        `}
+        onClick={() => setCurrentClick('TR')}
+      >
+        <div className={`${currentClick ? 'block' : 'hidden'}`} onClick={(e) => {
+          e.stopPropagation();
+          setCurrentClick('')
+        }
+        }>
+          x
+        </div>
+        <div className="text-center">
+          w
+        </div>
+
       </div>
-      <div className="w-1/2 h-1/2 absolute left-0 bottom-0 bg-yellow-200 hoverBig origin-bottom-left hover:z-20 z-10 rounded-tr-[100px]">
-        w
+
+      <div
+        className={`
+        absolute left-0 bottom-0 
+        bg-yellow-200 
+        origin-bottom-left 
+        ${currentClick === 'BL' ? 'w-full h-full z-30' : 'w-1/2 h-1/2 hover:z-20 z-10 hoverBig rounded-tr-[100px]'}
+        `}
+        onClick={() => setCurrentClick('BL')}
+      >
+        <div className={`${currentClick ? 'block' : 'hidden'}`} onClick={(e) => {
+          e.stopPropagation();
+          setCurrentClick('')
+        }
+        }>
+          x
+        </div>
+        <div className="text-center">
+          w
+        </div>
       </div>
-      <div className="w-1/2 h-1/2 absolute right-0 bottom-0 bg-green-200 hoverBig origin-bottom-right hover:z-20 z-10 rounded-tl-[100px]">
-        w
+
+      <div
+        className={`
+        absolute right-0 bottom-0 
+        bg-green-200 
+        origin-bottom-right 
+        ${currentClick === 'BR' ? 'w-full h-full z-30' : 'w-1/2 h-1/2 hover:z-20 z-10 hoverBig rounded-tl-[100px]'}
+        `}
+        onClick={() => setCurrentClick('BR')}
+      >
+        <div className={`${currentClick ? 'block' : 'hidden'}`} onClick={(e) => {
+          e.stopPropagation();
+          setCurrentClick('')
+        }
+        }>
+          x
+        </div>
+        <div className="text-center">
+          w
+        </div>
       </div>
+
       <div className="absolute top-[48.5%] left-[49.5%]">
         Hi
       </div>
