03fc717b6f32f8779545300c2b2d36487b2a5e5d
JarIsCoding <derpyderp1098@gmail.com>
2024-08-13 14:25:40 -0700

    hover feature working properly

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,16 @@
 export default function Home() {
   return (
     <div className="">
-      <div className="w-1/2 h-1/2 absolute left-0 top-0 bg-red-200">
+      <div className="w-1/2 h-1/2 absolute left-0 top-0 bg-red-200 hoverBig origin-top-left hover:z-20 z-10">
         w
       </div>
-      <div className="w-1/2 h-1/2 absolute right-0 top-0 bg-blue-200">
+      <div className="w-1/2 h-1/2 absolute right-0 top-0 bg-blue-200 hoverBig origin-top-right hover:z-20 z-10">
         w
       </div>
-      <div className="w-1/2 h-1/2 absolute left-0 bottom-0 bg-yellow-200">
+      <div className="w-1/2 h-1/2 absolute left-0 bottom-0 bg-yellow-200 hoverBig origin-bottom-left hover:z-20 z-10">
         w
       </div>
-      <div className="w-1/2 h-1/2 absolute right-0 bottom-0 bg-green-200">
+      <div className="w-1/2 h-1/2 absolute right-0 bottom-0 bg-green-200 hoverBig origin-bottom-right hover:z-20 z-10">
         w
       </div>
     </div>
